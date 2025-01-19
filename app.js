b3e5dc76da0137e276394b31f93162045c8e151d
Kk492000 <kunalkamthe200@gmail.com>
2025-01-19 19:38:50 +0530

    added headers in vercel.json file

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const placesPath = path.join(process.cwd(), "data", "places.json");
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, PUT", "OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET, PUT", "POST", "OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   next();
